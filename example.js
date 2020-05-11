5836587a1cb5f2cbf36693cccd85d4f16a4b6677
Glitch (chestnut-abrasive-butterkase) <none>
2020-05-11 06:51:06 +0000

    📈👖 Checkpoint ./example.js:7762666/904

--- example.js
+++ example.js
@@ -7,35 +7,25 @@ const app = express();
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
     console.log('QR RECEIVED', qr);
+    client.pupPage.screenshot({path: __dirname+'/public/qr.png'});
 });
 
 client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', msg => {
-    if (msg.body == '!ping') {
-        msg.reply('pong');
-    }
-});
-
 client.initialize();
 
-app.get('/', async (request, response) => {
-    try {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox']
-        });
-        const page = await browser.newPage();
-        await page.goto('https://developers.google.com/web/tools/puppeteer');
-        await page.screenshot({path: __dirname+'/public/puppeteer.png'});
-        await browser.close();
-        response.sendFile(__dirname+'/public/puppeteer.png');
-    } catch (error) {
-        console.log(error);
-    }
-});
+
 
 var listener = app.listen(process.env.PORT, function () {
     console.log('Your app is listening on port ' + listener.address().port);
+});
+
+app.get('/qr', async (request, response) => {
+    try {
+        response.sendFile(__dirname+'/public/qr.png');
+    } catch (error) {
+        console.log(error);
+    }
 });
