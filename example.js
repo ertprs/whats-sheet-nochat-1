3a528de852231d343c097564fbd05b33939a62cf
Glitch (chestnut-abrasive-butterkase) <none>
2020-05-11 06:40:00 +0000

    🚊🎣 Checkpoint ./package.json:7762666/32 ./example.js:7762666/926

--- example.js
+++ example.js
@@ -1,5 +1,8 @@
 const { Client } = require('whatsapp-web.js');
 const client = new Client({ puppeteer : { args: ['--no-sandbox'] } });
+const express = require('express');
+const app = express();
+
 
 client.on('qr', (qr) => {
     // Generate and scan this code with your phone
@@ -17,3 +20,22 @@ client.on('message', msg => {
 });
 
 client.initialize();
+
+app.get('/', async (request, response) => {
+    try {
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox']
+        });
+        const page = await browser.newPage();
+        await page.goto('https://developers.google.com/web/tools/puppeteer');
+        await page.screenshot({path: __dirname+'/public/puppeteer.png'});
+        await browser.close();
+        response.sendFile(__dirname+'/public/puppeteer.png');
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+var listener = app.listen(process.env.PORT, function () {
+    console.log('Your app is listening on port ' + listener.address().port);
+});
