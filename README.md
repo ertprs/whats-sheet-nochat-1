aeba5970ed46b3111eb80074c9bc8a717e19e336
Glitch (wa-aan) <none>
2020-11-15 11:47:54 +0000

    Import of aguskusmara/whats-sheet

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
-# whatsapp-multi
-A WhatsApp Web Multi clients that can be accessed simultaneously on multiple device browsers
+# whats-sheet
+A WhatsApp Web can be access from [google sheets](https://docs.google.com/spreadsheets/d)
 
-**NOTE:** I can't guarantee you will not be blocked by using this method, although it has worked for me. WhatsApp does not allow bots or unofficial clients on their platform, so this shouldn't be considered totally safe.
 
-## Links
+## Featured
 
-* [Reference](https://pedroslopez.me/whatsapp-web.js)
-* [Guide](https://waguide.pedroslopez.me/) _(work in progress)_
-* [GitHub](https://github.com/pedroslopez/whatsapp-web.js)
+* Send Message from google sheets
+* Check Eligible Number
+* Bulk send
 
-## Disclaimer
-This project is not affiliated, associated, authorized, endorsed by, or in any way officially connected with WhatsApp or any of its subsidiaries or its affiliates. The official WhatsApp website can be found at https://whatsapp.com. "WhatsApp" as well as related names, marks, emblems and images are registered trademarks of their respective owners.
--- example.js
+++ example.js
@@ -1,92 +1,92 @@
-const fs = require('fs');
-const { Client } = require('whatsapp-web.js');
-const express = require('express');
+const fs = require("fs");
+const { Client,MessageMedia } = require("whatsapp-web.js");
+const express = require("express");
 const app = express();
-const axios = require('axios');
-const http = require('http').createServer(app);
-const url = require('url');
-const io = require('socket.io')(http);
-const bodyParser = require('body-parser');
-const SESSION_FILE_PATH = './whatsapp-session.json';
+const axios = require("axios");
+const http = require("http").createServer(app);
+const url = require("url");
+const io = require("socket.io")(http);
+const bodyParser = require("body-parser");
+const SESSION_FILE_PATH = "./session.json";
 let sessionCfg;
 if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionCfg = require(SESSION_FILE_PATH);
 }
 
-const client = new Client({ puppeteer: { 
-  headless: true,
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--disable-accelerated-2d-canvas',
-    '--no-first-run',
-    '--no-zygote',
-    '--single-process', // <- this one doesn't works in Windows
-    '--disable-gpu'
-  ],
-}, 
-session: sessionCfg,
-restartOnAuthFail: true
+const client = new Client({
+  restartOnAuthFail: true,
+  puppeteer: {
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-accelerated-2d-canvas",
+      "--no-first-run",
+      "--no-zygote",
+      "--single-process", // <- this one doesn't works in Windows
+      "--disable-gpu"
+    ]
+  },
+  session: sessionCfg
 });
 
-app.use(bodyParser.json({ limit: '50mb' })); // for parsing application/json
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true })); // for parsing       application/x-www-form-urlencoded
+app.use(bodyParser.json({ limit: "50mb" })); // for parsing application/json
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true })); // for parsing       application/x-www-form-urlencoded
 app.use(express.json());
 
-io.on('connection', (socket) => {
-  console.log(io.engine.clientsCount + ' client connected');
-  io.emit('client', 'client connected');
-  
-  socket.on('disconnect', () => {
-    console.log(io.engine.clientsCount + ' disconect connected');
-    io.emit('client', 'disconect connected');
+io.on("connection", async socket => {
+  console.log(io.engine.clientsCount + " client connected");
+  io.emit("client", "client connected");
+
+  socket.on("disconnect", () => {
+    console.log(io.engine.clientsCount + " disconect connected");
   });
 });
 
 // listen for requests!
 const listener = http.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
 
-client.on('qr', (qr) => {
+client.on("qr", qr => {
   // Generate and scan this code with your phone
-  console.log('QR RECEIVED', qr);
-  client.pupPage.screenshot({path: __dirname+'/public/qr.png'});
-  io.emit('qr', qr);
+  console.log("QR RECEIVED", qr);
+  client.pupPage.screenshot({ path: __dirname + "/public/qr.png" });
+  io.emit("qr", qr);
 });
 
-client.on('authenticated', (session) => {
-  console.log('AUTHENTICATED', session);
-  sessionCfg=session;
-  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+client.on("authenticated", session => {
+  console.log("AUTHENTICATED", session);
+  sessionCfg = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function(err) {
     if (err) {
       console.error(err);
     }
   });
 });
 
-client.on('auth_failure', msg => {
+client.on("auth_failure", msg => {
   // Fired if session restore was unsuccessfull
-  console.error('AUTHENTICATION FAILURE', msg);
+  console.error("AUTHENTICATION FAILURE", msg);
 });
 
-client.on('disconnected', (reason) => {
-  console.log('Client was logged out', reason);
-  io.emit('client', reason);
-  if(reason === 'UNPAIRED'){
+client.on("disconnected", reason => {
+  console.log("Client was logged out", reason);
+  io.emit("client", reason);
+  if (reason === "UNPAIRED") {
     fs.unlinkSync(SESSION_FILE_PATH, function(err) {
-      if(err) return console.log(err);
-      console.log('Session file deleted!');
+      if (err) return console.log(err);
+      console.log("Session file deleted!");
     });
   }
   client.destroy();
   client.initialize();
 });
 
-client.on('ready', () => {
-  console.log('Client is ready!');
-  io.emit('client', 'Client is ready!');
+client.on("ready", () => {
+  console.log("Client is ready!");
+  io.emit("client", "Client is ready!");
 });
 
 client.initialize();
@@ -95,15 +95,15 @@ client.initialize();
 //   io.emit('message', msg);
 // });
 
-client.on('message_create', (msg) => {
+client.on("message_create", msg => {
   // Fired on all message creations, including your own
-  io.emit('message', msg);
+  io.emit("message", msg);
   if (msg.fromMe) {
     // do stuff here
   }
 });
 
-client.on('message_ack', (msg, ack) => {
+client.on("message_ack", (msg, ack) => {
   /*
       == ACK VALUES ==
       ACK_ERROR: -1
@@ -114,127 +114,176 @@ client.on('message_ack', (msg, ack) => {
       ACK_PLAYED: 4
   */
 
-  if(ack == 3) {
+  if (ack == 3) {
     // The message was read
-    io.emit('message_read', msg);
+    io.emit("message_read", msg);
   }
 });
 
-app.get('/', async(req, res) => {
-  res.sendFile(__dirname + '/view/index.html');
+app.get("/", async (req, res) => {
+  res.sendFile(__dirname + "/view/index.html");
 });
 
-app.get('/qr', async (req, res) => {
+app.get("/qr", async (req, res) => {
   try {
-    res.sendFile(__dirname + '/public/qr.png');
+    res.sendFile(__dirname + "/public/qr.png");
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get('/info', (req, res) => {
+app.get("/info", async (req, res) => {
   if (client.info) {
     res.send(client.info);
-  }
-  else
-  {
-    res.send({msg: 'No Client Connected! <a href=\'qr\'>Scan QR</a> to Start'});
+  } else {
+    res.send({ msg: "No Client Connected! <a href='qr'>Scan QR</a> to Start" });
   }
 });
 
-app.get('/chats', async(req, res) => {
+app.get("/chats", async (req, res) => {
   try {
     const chats = await client.getChats();
     res.send(chats);
-  }
-  catch(e) {
-    res.status(500).send({msg: 'Get Chats Error!'});
+  } catch (e) {
+    res.status(500).send({ msg: "Get Chats Error!" });
     console.log(e.message);
     //throw new Error(req.url);
   }
 });
 
-app.get('/chats/:date', async(req, res) => {
+app.get("/chats/:date", async (req, res) => {
   try {
     const chats = await client.getChats();
-    let dateStr = req.params.date.substring(0, 4) + '.' + req.params.date.substring(4,6) + '.' + req.params.date.substring(6,8);
+    let dateStr =
+      req.params.date.substring(0, 4) +
+      "." +
+      req.params.date.substring(4, 6) +
+      "." +
+      req.params.date.substring(6, 8);
     let dateFrom = new Date(dateStr);
-    let filteredChat = chats.filter(c => c.timestamp >= (dateFrom.getTime() / 1000));
+    let filteredChat = chats.filter(
+      c => c.timestamp >= dateFrom.getTime() / 1000
+    );
     res.send(filteredChat);
-  }
-  catch(e) {
-    res.status(500).send('Get Chats By Date Error!');
+  } catch (e) {
+    res.status(500).send("Get Chats By Date Error!");
     console.log(e.message);
     //throw new Error(req.url);
   }
 });
 
-app.get('/chats/:dateFrom/:dateTo', async(req, res) => {
+app.get("/chats/:dateFrom/:dateTo", async (req, res) => {
   try {
     const chats = await client.getChats();
-    let dateStr = req.params.dateFrom.substring(0, 4) + '.' + req.params.dateFrom.substring(4,6) + '.' + req.params.dateFrom.substring(6,8);
+    let dateStr =
+      req.params.dateFrom.substring(0, 4) +
+      "." +
+      req.params.dateFrom.substring(4, 6) +
+      "." +
+      req.params.dateFrom.substring(6, 8);
     let dateFrom = new Date(dateStr);
-    dateStr = req.params.dateTo.substring(0, 4) + '.' + req.params.dateTo.substring(4,6) + '.' + req.params.dateTo.substring(6,8);
+    dateStr =
+      req.params.dateTo.substring(0, 4) +
+      "." +
+      req.params.dateTo.substring(4, 6) +
+      "." +
+      req.params.dateTo.substring(6, 8);
     let dateTo = new Date(dateStr);
-    let filteredChat = chats.filter(c => c.timestamp >= (dateFrom.getTime() / 1000) && c.timestamp <= (dateTo.getTime() / 1000));
+    let filteredChat = chats.filter(
+      c =>
+        c.timestamp >= dateFrom.getTime() / 1000 &&
+        c.timestamp <= dateTo.getTime() / 1000
+    );
     res.send(filteredChat);
-  }
-  catch(e) {
-    res.status(500).send('Get Chats By Date From To Error!');
+  } catch (e) {
+    res.status(500).send("Get Chats By Date From To Error!");
     console.log(e.message);
     //throw new Error(req.url);
   }
 });
 
-app.get('/chat/:id', async(req, res) => {
+app.get("/chat/:id", async (req, res) => {
   try {
-    let number = req.params.id + (req.params.id.includes('-') ? '@g.us' : '@c.us');
+    let number =
+      req.params.id + (req.params.id.includes("-") ? "@g.us" : "@c.us");
     const chat = await client.getChatById(number);
     await chat.fetchMessages().then(f => res.send(f));
-  }
-  catch(e) {
-    res.status(500).send('Get Chat by Id Error');
+  } catch (e) {
+    res.status(500).send("Get Chat by Id Error");
     throw new Error(req.url);
   }
 });
 
-app.get('/getimage/:id/:msgid', async(req, res) => {
+app.get("/getimage/:id/:msgid", async (req, res) => {
   try {
-    let number = req.params.id + (req.params.id.includes('-') ? '@g.us' : '@c.us');
+    let number =
+      req.params.id + (req.params.id.includes("-") ? "@g.us" : "@c.us");
     let msgId = req.params.msgid;
-    await client.getChatById(number).then(async(c)=>{
-      let searchOptions = { limit: 100 };
-      await c.fetchMessages(searchOptions).then(messages=>{
-        messages.forEach(async (msg, index)=>{
-          if(msg.id.id==msgId){
-            await msg.downloadMedia().then(value=>{
-              res.send(value);
-            }).catch(err=>{
-              res.status(500).send('Error Downloading Image in Get Image!');
+    await client
+      .getChatById(number)
+      .then(async c => {
+        let searchOptions = { limit: 100 };
+        await c
+          .fetchMessages(searchOptions)
+          .then(messages => {
+            messages.forEach(async (msg, index) => {
+              if (msg.id.id == msgId) {
+                await msg
+                  .downloadMedia()
+                  .then(value => {
+                    res.send(value);
+                  })
+                  .catch(err => {
+                    res
+                      .status(500)
+                      .send("Error Downloading Image in Get Image!");
+                  });
+              }
             });
-          }
-        });
-      }).catch(err => {
-        res.status(500).send('Error Fetch Messages in Get Image!');
+          })
+          .catch(err => {
+            res.status(500).send("Error Fetch Messages in Get Image!");
+          });
+      })
+      .catch(err => {
+        res.status(500).send("Error Chat not Found in Get Image!");
       });
-    }).catch(err => {
-      res.status(500).send('Error Chat not Found in Get Image!');
-    });;
-
   } catch (e) {
-    res.status(500).send('Error Get Image!');
+    res.status(500).send("Error Get Image!");
   }
 });
 
-app.get('/send/:id/:message', function(req, res) {
+app.get("/send/:id/:message", function(req, res) {
   try {
-    let number = req.params.id + (req.params.id.includes('-') ? '@g.us' : '@c.us');
+    let number =
+      req.params.id + (req.params.id.includes("-") ? "@g.us" : "@c.us");
     let message = req.params.message;
     res.send(client.sendMessage(number, message));
-  }
-  catch(e) {
-    res.status(500).send('Send Message Error');
+  } catch (e) {
+    res.status(500).send("Send Message Error");
     throw new Error(req.url);
+  }
+});
+
+app.get("/status", async function(req, res) {
+  try {
+    const statuswa = await client
+      .getState()
+      .then(response => {
+        res.status(200).json({
+          status: true,
+          response: response
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          status: false,
+          message: "Your not a loggin"
+        });
+      });
+  } catch (e) {
+    res.status(500).send("Your not a loggin");
+    // throw new Error(req.url);
   }
 });
 
@@ -243,77 +292,82 @@ const checkRegisteredNumber = async function(number) {
   return isRegistered;
 };
 
-app.post('/send',async function(req, res) {
+app.post("/send", async function(req, res) {
   try {
-    let number = req.body.number + (req.body.number.includes('-') ? '@g.us' : '@c.us');
+    let number =
+      req.body.number + (req.body.number.includes("-") ? "@g.us" : "@c.us");
     let message = req.body.message;
     let isRegisteredNumber = await checkRegisteredNumber(number);
     if (!isRegisteredNumber) {
       return res.status(422).json({
         status: false,
-        message: 'The number is not registered'
+        message: "The number is not registered"
       });
     }
-    
-    client.sendMessage(number, message).then(response => {
-      res.status(200).json({
-        status: true,
-        response: response
-      });
-    }).catch(err => {
-      res.status(500).json({
-        status: false,
-        message: 'Your not a loggin'
-      });
-    });
-    // res.send(client.sendMessage(number, message));
-  }
 
-  catch(e) {
+    client
+      .sendMessage(number, message)
+      .then(response => {
+        res.status(200).json({
+          status: true,
+          response: response
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          status: false,
+          message: "Your not a loggin"
+        });
+      });
+    // res.send(client.sendMessage(number, message));
+  } catch (e) {
     console.error(e);
-    res.status(500).json({message: 'Your not a loggin'});
+    res.status(500).json({ message: "Your not a loggin" });
     throw new Error(req.url);
   }
 });
 
-app.post('/send-image',async function (req, res) {
+app.post("/send-image", async function(req, res) {
   try {
     console.log(req);
-    let number = req.body.number + (req.body.number.includes('-') ? '@g.us' : '@c.us');
+    let number =
+      req.body.number + (req.body.number.includes("-") ? "@g.us" : "@c.us");
     let mime = req.body.mime.toString();
     const data = req.body.src.toString();
     let filename = req.body.filename.toString();
     let thumbnail = req.body.thumbnail.toString();
     let caption = req.body.caption.toString();
-    let option = { attachment: thumbnail, caption:caption};
-    const img = new MessageMedia(mime, data,filename);
-    await client.sendMessage(number, img, caption).then(value=>{
-      res.send(value);
-    }).catch(error=>{
-      res.status(500).send('Post Message Error');
-      throw new Error(req.url);
-    });
-  }
-  catch (e) {
-    res.status(500).send('Post Message Error');
+    let option = { attachment: thumbnail, caption: caption };
+    const img = new MessageMedia(mime, data, filename);
+    await client
+      .sendMessage(number, img, caption)
+      .then(value => {
+        res.send(value);
+      })
+      .catch(error => {
+        res.status(500).send("Post Message Error");
+        throw new Error(req.url);
+      });
+  } catch (e) {
+    res.status(500).send("Post Message Error");
     throw new Error(req.url);
   }
 });
 
-app.get('/contacts', async(req, res) => {
+app.get("/contacts", async (req, res) => {
   try {
     const contacts = await client.getContacts();
     res.send(contacts);
-  }
-  catch(e) {
-    res.status(500).send({msg: 'Get Contacts Error!'});
+  } catch (e) {
+    res.status(500).send({ msg: "Get Contacts Error!" });
     console.log(e.message);
   }
 });
 
 // Send media
-app.post('/send-media', async (req, res) => {
-  let number = req.body.number + (req.body.number.includes('-') ? '@g.us' : '@c.us');
+app.post("/send-media", async (req, res) => {
+  let number =
+    req.body.number + (req.body.number.includes("-") ? "@g.us" : "@c.us");
   const caption = req.body.caption;
   const fileUrl = req.body.file;
 
@@ -321,30 +375,60 @@ app.post('/send-media', async (req, res) => {
   if (!isRegisteredNumber) {
     return res.status(422).json({
       status: false,
-      message: 'The number is not registered'
+      message: "The number is not registered"
     });
   }
   let mimetype;
-  const attachment = await axios.get(fileUrl, {
-    responseType: 'arraybuffer'
-  }).then(response => {
-    mimetype = response.headers['content-type'];
-    return response.data.toString('base64');
-  });
-
-  const media = new MessageMedia(mimetype, attachment, 'Media');
-
-  client.sendMessage(number, media, {
-    caption: caption
-  }).then(response => {
-    res.status(200).json({
-      status: true,
-      response: response
+  const attachment = await axios
+    .get(fileUrl, {
+      responseType: "arraybuffer"
+    })
+    .then(response => {
+      mimetype = response.headers["content-type"];
+      return response.data.toString("base64");
     });
-  }).catch(err => {
+
+  const media = new MessageMedia(mimetype, attachment, "Media");
+
+  client
+    .sendMessage(number, media, {
+      caption: caption
+    })
+    .then(response => {
+      res.status(200).json({
+        status: true,
+        response: response
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        status: false,
+        response: err
+      });
+    });
+});
+
+app.get("/cek", async (req, res) => {
+  let number =
+    req.body.number + (req.body.number.includes("-") ? "@g.us" : "@c.us");
+  try {
+    let isRegisteredNumber = await checkRegisteredNumber(number);
+    if (!isRegisteredNumber) {
+      return res.status(422).json({
+        status: false,
+        message: "The number is not registered"
+      });
+    }
+    if (isRegisteredNumber) {
+      return res.status(200).json({
+        status: true,
+        message: "The number registered"
+      });
+    }
+  } catch (error) {
     res.status(500).json({
       status: false,
       response: err
     });
-  });
+  }
 });
